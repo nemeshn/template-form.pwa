a67dc242eaef9b519a9dd9bb7e34ce4575986276
Gabriel F7 <63380935+gabrielf7@users.noreply.github.com>
2021-02-11 14:57:05 -0300

    Adding function: acquireSelecting, renderLiImage, renderUlImages
    Adding function: acquireSelecting, renderLiImage, renderUlImages

--- frontend/src/components/ImageScroller/index.jsx
+++ frontend/src/components/ImageScroller/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import Image from '../Image';
+import Image from '../Image';
 // import ButtonImage from '../ButtonImage';
 import ManipularEvento from './ManipularEvento';
 
@@ -16,17 +16,77 @@ class ImageScroller extends React.Component {
     };
   }
 
-  obterSelecionado() {
+  acquireSelecting() {
     const { elements } = this.props;
     const { manipularEvento } = this.state;
     return elements[
       manipularEvento.index
     ];
   }
+
+  renderLiImage(entry, index) {
+    const { axisY, file } = this.props;
+    // let iAxisY = axisY ? axisY : 0;
+    const iAxisY = axisY || 0;
+
+    return (
+      <li
+        style={{
+          zIndex: '-1',
+          position: 'absolute',
+          paddingTop: '8px',
+          marginLeft: `${index * 140}px`,
+        }}
+        key={index + entry.toString()}
+      >
+        <Image
+          axisX={entry.index}
+          axisY={iAxisY}
+          width={140}
+          height={140}
+          backgroundHeight={280}
+          file={file}
+        />
+      </li>
+    );
+  }
+
+  renderUlImages() {
+    const { elements } = this.props;
+    const { manipularEvento } = this.state;
+    const tempMS = manipularEvento.toqueEmExecucao ? '100ms' : '800ms';
+
+    const styled = {
+      margin: '0',
+      padding: '0',
+      position: 'relative',
+      width: `${elements.length * 140}px`,
+      left: `${manipularEvento.left}px`,
+      listStyleType: 'none',
+
+      WebkitTransitionDuration: tempMS, /* Safari e Chrome */
+      MsTransitionDuration: tempMS, /* IE */
+      MozTransitionDuration: tempMS, /* Firefox */
+      OTransitionDuration: tempMS, /* Opera */
+      transitionDuration: tempMS, /* Nativa do W3C */
+    };
+
+    const listElements = elements.map(
+      (entry, index) => this.renderLiImage(entry, index),
+    );
+
+    return (
+      <ul style={styled}>
+        {listElements}
+      </ul>
+    );
+  }
 }
 ImageScroller.propTypes = {
-  elements: PropTypes.string.isRequired,
-  selected: PropTypes.string.isRequired,
+  elements: PropTypes.arrayOf.isRequired,
+  selected: PropTypes.number.isRequired,
+  axisY: PropTypes.number.isRequired,
+  file: PropTypes.string.isRequired,
 };
 
 export default ImageScroller;
