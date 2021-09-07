23478ceb04a2a6da06aa9fa41fa5dd68925ee32b
JG <diasusb@hotmail.com>
2021-09-07 03:31:10 -0300

    Add: Component App has verification of existing user registration

--- frontend/src/models/Users.js
+++ frontend/src/models/Users.js
@@ -1,11 +1,15 @@
 /* eslint-disable prefer-destructuring */
 import Avatar from './Avatar';
+import Repository from '../infrastructure/Respository';
+
+const repository = new Repository();
+const acquireAll = Avatar.acquireAll()[0];
 
 class Users {
   constructor() {
     this.name = '';
     this.genero = '';
-    this.avatar = Avatar.acquireAll()[0];
+    this.avatar = acquireAll;
     // () => {
     //   const { acquireAll } = Avatar.acquireAll();
     //   return acquireAll[0];
@@ -22,6 +26,28 @@ class Users {
 
   validateGenero() {
     return ['m', 'f'].some((param) => this.genero === param);
+  }
+
+  save(callback) {
+    repository.save(this, callback);
+  }
+
+  static acquire(sucess, fail) {
+    repository.acquire((json) => {
+      // eslint-disable-next-line prefer-const
+      let user = new Users();
+      user.name = json.name;
+      user.genero = json.genero;
+      user.avatar = new Avatar(
+        json.avatar.index,
+        json.avatar.description,
+      );
+      sucess(user);
+    }, fail);
+  }
+
+  toString() {
+    return `${this.name}, ${this.avatar.toString()}`;
   }
 }
 
